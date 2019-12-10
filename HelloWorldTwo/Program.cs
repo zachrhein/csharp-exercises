da132da3c1f3c38f7ed73997bee4bd5be5cdc77a
Zach Rhein <zachrhein@me.com>
2019-12-10 13:37:49 -0600

    initial commit

---
+++ HelloWorldTwo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorldTwo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a width: ");
+            int width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            int area = width * height;
+            Console.WriteLine("The area of the recanglt is " + area);
+        }
+    }   
+}
+ 
---
+++ IfStatements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm doing a giveaway!");
+            Console.WriteLine("Please pick a number from 1-10. ");
+            string user = Console.ReadLine();
+
+            string winner = "";
+            if (user == "3")
+               winner = "You have won a new car!";
+            else if (user == "4")
+                winner = "you were so close! Sorry, you didn't win";
+            else
+            { 
+                winner = "sorry, you did not win this time";
+            }
+
+            Console.WriteLine("You entered {0}, {1}", user, winner);
+            Console.ReadLine();
+
+        }
+    }
+}
